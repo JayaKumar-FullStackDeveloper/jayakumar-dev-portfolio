a58248c451e755d97d209bd7baedeba341069397
Jayakumar <113231981+JayaKumar-FullStackDeveloper@users.noreply.github.com>
2025-01-29 20:52:15 +0530

    Experiance Updated experiance.jsx

--- src/components/experiance.jsx
+++ src/components/experiance.jsx
@@ -6,7 +6,7 @@ function Experiance() {
             <ul className="space-y-4">
                 <li className="group relative grid transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 p-4 rounded-lg overflow-hidden">
                     <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-600/15 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
-                    <header className="z-10 mb-2 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2024 - Present">2024 - Present</header>
+                    <header className="z-10 mb-2 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2024 - Present">Mar 2024 - Dec 2024</header>
                     <div className="z-10 sm:col-span-6">
                         <h3 className="font-medium leading-snug text-slate-200">
                             <div>
@@ -34,6 +34,36 @@ function Experiance() {
                         </div>
                     </div>
                 </li>
+                <li className="group relative grid transition-all sm:grid-cols-8 sm:gap-8 md:gap-4 p-4 rounded-lg overflow-hidden">
+                    <div className="absolute -inset-x-4 -inset-y-4 z-0 hidden transition motion-reduce:transition-none lg:-inset-x-6 lg:block lg:group-hover:bg-slate-600/15 lg:group-hover:shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] lg:group-hover:drop-shadow-lg"></div>
+                    <header className="z-10 mb-2 text-sm font-semibold uppercase tracking-wide text-slate-500 sm:col-span-2" aria-label="2024 - Present">Jan 2024 - Present</header>
+                    <div className="z-10 sm:col-span-6">
+                        <h3 className="font-medium leading-snug text-slate-200">
+                            <div>
+                                <a className="inline-flex items-baseline font-medium leading-tight text-slate-200 hover:text-teal-300 focus-visible:text-teal-300 group/link text-base" href="https://www.softsuave.com/" target="_blank" rel="noreferrer noopener" aria-label="UI Engineer Co-op at Apple (opens in a new tab)">
+                                    <span className="absolute -inset-x-4 -inset-y-2.5 hidden rounded md:-inset-x-6 md:-inset-y-4 lg:block"></span>
+                                    <span>UI/UX Frontend Developer | Soft Suave Technologies
+                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true">
+                                            <path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path>
+                                        </svg>
+                                    </span>
+                                </a>
+                            </div>
+                        </h3>
+                        <p className="mt-2 text-base leading-normal text-justify">
+                            I have hands-on experience in UI/UX design, working alongside senior designers and developers. I am responsible for all stages of the design process, including research, wireframing, prototyping, and implementation. My focus is on creating intuitive, user-centered designs that ensure seamless, responsive experiences across devices. Additionally, I conduct usability testing and refine designs based on user feedback to improve functionality and enhance overall user satisfaction.
+                        </p>
+                        <div className="flex flex-wrap pt-3 gap-2">
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">UI/UX</div>
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">Adobe Photoshop</div>
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">Adobe Illustrator</div>
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">React Js</div>
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">HTML</div>
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">CSS</div>
+                            <div className="flex items-center rounded-full bg-teal-400/10 px-3 py-1 text-xs font-medium leading-4 text-teal-200 ">Canva</div>
+                        </div>
+                    </div>
+                </li> 
                 {/* Add more experience list items here */}
             </ul>
             <div className="mt-6">
