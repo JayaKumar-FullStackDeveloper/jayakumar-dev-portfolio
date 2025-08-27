730f0f80eaa26ae6eeba9198ab63520c8bb389d2
Jayakumar <jayakumarjayakrishnanjk@gmail.com>
2025-08-27 23:15:27 +0530

    Just Added

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -1,20 +1,31 @@
 import React from "react";
-import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaDocker, FaAws, FaJs , FaGitAlt} from "react-icons/fa";
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs, FaDocker, FaAws, FaJs, FaGitAlt } from "react-icons/fa";
 import { SiTailwindcss, SiMongodb, SiExpress, SiFlutter, SiKubernetes, SiMysql } from "react-icons/si";
 import { IoLogoFigma } from "react-icons/io5";
 
 const IconCard = ({ Icon }) => {
   return (
-    <div className="flex items-center justify-center w-14 h-14 dark:bg-gray-900 rounded-full transform transition-transform duration-300 hover:scale-110 hover:bg-gray-300 dark:hover:bg-gray-700 z-50">
-      <Icon className="text-3xl text-blue-700 hover:text-slate-50 z-50 relative" />
+    <div
+      className="group flex items-center justify-center w-14 h-14 
+    dark:bg-gray-900 rounded-full transform transition-transform duration-300 
+    hover:scale-110 dark:hover:bg-gray-700 z-50"
+    >
+      <Icon
+        className="text-3xl text-white transition-all duration-300 
+      group-hover:text-[#6167f8]"
+        style={{
+          transition: "all 0.3s ease",
+        }}
+      />
     </div>
+
   );
 };
 
 export const IconMarquee = () => {
   const icons = [
-    FaReact, SiTailwindcss, FaHtml5, FaCss3Alt, FaJs, FaNodeJs, SiExpress, SiMongodb,SiMysql,
-    FaDocker, SiKubernetes, SiFlutter, FaAws,IoLogoFigma,FaGitAlt
+    FaReact, SiTailwindcss, FaHtml5, FaCss3Alt, FaJs, FaNodeJs, SiExpress, SiMongodb, SiMysql,
+    FaDocker, SiKubernetes, SiFlutter, FaAws, IoLogoFigma, FaGitAlt
   ];
 
   return (
